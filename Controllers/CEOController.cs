9ccaa862cd2d31a1deca09c3b5e789f607791ad1
EdenGenetTasama <edentasama19@gmail.com>
2022-01-18 16:37:59 +0200

    add CEO controllers

---
+++ Controllers/CEOController.cs
@@ -0,0 +1,33 @@
+﻿using HotelWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelWebApplication.Controllers
+{
+    public class CEOController : Controller
+    {
+
+  
+
+        // GET: CEO
+        public ActionResult GetCEOName()
+        {
+            ViewBag.Name =CEO.CEOlist[CEO.CEOlist.Count-1];
+            return View();
+        }
+
+        // GET: CEO/AllInfo
+        public ActionResult AllInfo()
+        {
+            ViewBag.AllInfo =CEO.CEOlist[CEO.CEOlist.Count-1];
+            return View();
+        }
+
+    
+
+
+    }
+}
---
+++ Models/CEO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelWebApplication.Models
+{
+    public class CEO
+    {
+        public CEO(int id, string fullName, int payment, int age)
+        {
+            Id=id;
+            FullName=fullName;
+            Payment=payment;
+            Age=age;
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Payment { get; set; }
+        public int Age { get; set; }
+
+        static public List<CEO> CEOlist = new List<CEO>()
+        {
+        new CEO(0 , "EdenTasama", 20000 , 25 ),
+        new CEO(1 , "Daniel Tal" , 30000 , 26),
+        new CEO(2 , "Oshra Tasama" , 40000 , 29 ),
+
+        };
+    }
+
+}
